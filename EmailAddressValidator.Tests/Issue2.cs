37599c6d013393cc6652ef147d54f60686277603
Talles L <talleslasmar@gmail.com>
2015-07-31 18:09:28 -0300

    unit tests with #2 examples

---
+++ EmailAddressValidator.Tests/Issue2.cs
@@ -0,0 +1,24 @@
+﻿namespace EmailAddressValidation.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Examples from issue #2.
+    /// (https://github.com/tallesl/EmailAddressValidator/issues/2)
+    /// </summary>
+    [TestClass]
+    public class Issue2
+    {
+        [TestMethod]
+        public void FalsePositivesFromIssue2()
+        {
+            Assert.IsTrue(EmailAddressValidator.IsValid("fooé@bar.com"));
+            Assert.IsTrue(EmailAddressValidator.IsValid("fooée@bar.com"));
+            Assert.IsTrue(EmailAddressValidator.IsValid("con@firm.ed."));
+            Assert.IsTrue(EmailAddressValidator.IsValid("con@firm.ed..."));
+            Assert.IsTrue(EmailAddressValidator.IsValid("con@firm.ed.++"));
+            Assert.IsTrue(EmailAddressValidator.IsValid("con@firm.ed---"));
+            Assert.IsTrue(EmailAddressValidator.IsValid("con@firm.ed--///***-test"));
+        }
+    }
+}
