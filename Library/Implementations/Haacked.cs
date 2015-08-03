e932fd9ce24e0f16067210453b53f12a420ed9b8
Talles L <talleslasmar@gmail.com>
2015-08-03 22:59:22 +0000

    removing sneaky tabs

--- Library/Implementations/Haacked.cs
+++ Library/Implementations/Haacked.cs
@@ -3,9 +3,9 @@
     using System.Text.RegularExpressions;
 
     internal static class HaackedRegex
-	{
+    {
         public static readonly Regex Regex = new Regex(
             @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$",
             RegexOptions.IgnoreCase);
-	}
+    }
 }
